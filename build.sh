751d48782719eebff96d17d70cff69ef084b7829
Eric Ireland <artanin@Erics-MacBook-Pro.local>
2024-07-11 13:35:00 -0100

    Build system changes for osx Add osx-buildnotes.txt so others can duplicate the provided pre-built libaries. Modify csproject files to remove x86 restriction and just target .net8.0 Modify Yafc/Yafc.csproj to be osx-arm64 aware and to import the prebuilt libaries. Create Yafc/lib/osx-arm64 and provide SDL and lua libaries to make running easier. Modify build.sh to build for OSX-arm64 and rename the old OSX build to specify intel.

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 rm -rf Build
 dotnet publish Yafc/Yafc.csproj -r win-x64 -c Release -o Build/Windows
 dotnet publish Yafc/Yafc.csproj -r osx-x64 --self-contained false -c Release -o Build/OSX
+dotnet publish Yafc/Yafc.csproj -r osx-arm64 --self-contained false -c Release -o Build/OSX-arm64
+
 dotnet publish Yafc/Yafc.csproj -r linux-x64 --self-contained false -c Release -o Build/Linux
 
 pushd Build
 tar czf Linux.tar.gz Linux
-tar czf OSX.tar.gz OSX
+tar czf OSX-intel.tar.gz OSX
+tar czf OSX-arm64.tar.gz OSX-arm64
 zip -r Windows.zip Windows
 popd
 
